cfacba96176d8925595c508b3e8fe4f1c32fba33
Jeanne d'Arc NYIRANWIZA <njoanc@gmail.com>
2024-10-22 14:36:20 +0200

    fix():lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
+    quotes: ["off"],
     semi: ["off"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
